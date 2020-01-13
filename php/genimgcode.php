e704af249e4aaf37f9a5737ee28c992142c8d88f
mark l chaves <mark@marklchaves.com>
2020-01-13 17:57:54 +0800

    Clipboard, styles, update HTML gen

--- php/genimgcode.php
+++ php/genimgcode.php
@@ -1,41 +1,21 @@
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Gallery from Folder Demo</title>
-<style type="text/css">
-<!--
-* {box-sizing: border-box;}
-
-body {
-	font-family: Arial, Helvetica, sans-serif;
-}
-
-section {
-	display: block;
-	max-width: 80%;
-	padding: 0 3%;
-}
-
-li{
-	list-style-type:none;
-	margin-right:10px;
-	margin-bottom:10px;
-	float:left;
-}
--->
-</style>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>The Code - Generate Img Code</title>
+	<link rel="stylesheet" href="genimgcode.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js"></script>
 </head>
 
 <?php
 
 $dirname = (empty($_POST['directory'])) ? 'wp-content/uploads/2020/01/' : $_POST['directory'];
-
 $assetsdir = (empty($_POST['assets'])) ? '/assets/images/' : $_POST['assets'];
+$ignore = array(".", "..", ".DS_Store");
 
 function convert_filename_alt_title($str)
-{ 
+{
 	// Convert the filename to an alt and title.
 	// Replace hyphens with a space.
 	$altstr = preg_replace('"-"', ' ', $str);
@@ -46,46 +26,73 @@ function convert_filename_alt_title($str)
 ?>
 
 <body>
-<section id="liquid">
-	<h2>Liquid Front Matter</h2>
-	<?php
+	<header>
+		<h1>Generate Img Code</h1>
+		<h2>The Code</h2>
+	</header>
+	<section id="liquid">
+		<h2>Liquid Front Matter</h2>
+		<?php
 		$images2 = scandir($dirname); // To do: Error handling.
-		$ignore2 = array(".", "..");
-		echo "<pre>\n";
-		foreach($images2 as $curimg2){
-			if(!in_array($curimg2, $ignore2)) {
+		echo "<pre id='liquid-code'>\n";
+		foreach ($images2 as $curimg2) {
+			if (!in_array($curimg2, $ignore)) {
 				$altstr = (empty($_POST['alt'])) ? convert_filename_alt_title($curimg2) : $_POST['alt'];
 				$title = $altstr;
-				
+
 				/* Spaces here are important. */
-				echo "\n  - url: $assetsdir$curimg2\n    image_path: $assetsdir$curimg2\n    alt: \"$altstr\"\n    title: \"$title\"\n";
+				echo "\n  - url: $assetsdir$curimg2\n    image_path: $assetsdir$curimg2\n    alt: \"$altstr\"\n    title: \"$title\"";
 			}
-		} 
+		}
 		echo "</pre>\n";
-	echo "\n";							
-	?>
-</section>
-<hr>
-<section id="images">
-<h2>Images</h2>
-<p>View source to get HTML code.</p>
-<ul>
-	<?php
-		$images = scandir($dirname); // To do: Error handling.
-		$ignore = array(".", "..");
-		foreach($images as $curimg){
-			if(!in_array($curimg, $ignore)) {
-				$altstr = convert_filename_alt_title($curimg);
-				$title = $altstr;
-				echo "\n\t<li><a href=\"$dirname$curimg\"><img src='$dirname$curimg' alt='$altstr' title='$title' /></a></li>";
+		echo '<button class="button" onclick="alert(\'Copied\')" class="button" id="liquid-button" data-clipboard-target="#liquid-code">Copy</button>';
+		echo "\n\n";
+		?>
+	</section>
+
+	<section id="html">
+		<h2>HTML</h2>
+			<?php
+			$images = scandir($dirname); // To do: Error handling.
+			echo "<pre id='html-code'>\n";
+			foreach ($images as $curimg) {
+				if (!in_array($curimg, $ignore)) {
+					$altstr = (empty($_POST['alt'])) ? convert_filename_alt_title($curimg) : $_POST['alt'];
+					$title = $altstr;
+					$dispstr = htmlspecialchars("<a href=\"$dirname$curimg\">\n  <img src=\"$dirname$curimg\"\n  alt=\"$altstr\"\n  title=\"$title\"></a>", ENT_QUOTES);
+					echo "\n" . $dispstr;
+				}
 			}
-		} 
-		echo "\n\n"; 
-	?>
-</ul>
-<?php
-echo "\n\n"; 
-?>
-</section>
+			echo "</pre>\n";
+			echo '<button class="button" onclick="alert(\'Copied\')" class="button" id="html-button" data-clipboard-target="#html-code">Copy</button>';
+			echo "\n\n";
+			?>
+	</section>
+	<section id="images">
+		<h2>Images</h2>
+		<ul>
+			<?php
+			$images = scandir($dirname); // To do: Error handling.
+			foreach ($images as $curimg) {
+				if (!in_array($curimg, $ignore)) {
+					$altstr = (empty($_POST['alt'])) ? convert_filename_alt_title($curimg) : $_POST['alt'];
+					$title = $altstr;
+					echo "\n\t<li><a href=\"$dirname$curimg\"><img src=\"$dirname$curimg\" alt=\"$altstr\" title=\"$title\" /></a></li>";
+				}
+			}
+			echo "\n\n";
+			?>
+		</ul>
+		<?php
+		echo "\n\n";
+		?>
+	</section>
+	<script>
+		(function() {
+			new ClipboardJS('#liquid-button');
+			new ClipboardJS('#html-button');
+		})();
+	</script>
 </body>
+
 </html>
